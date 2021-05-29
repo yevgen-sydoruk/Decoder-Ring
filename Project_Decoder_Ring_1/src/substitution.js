0cdffe40f7e1c66367ea4e7357b0502f971fdec9
Yevgen Sydoruk <yevgen.sydoruk@gmail.com>
2021-05-29 09:39:38 -0500

    Substitution Main code and tests done

--- Project_Decoder_Ring_1/src/substitution.js
+++ Project_Decoder_Ring_1/src/substitution.js
@@ -4,15 +4,34 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+    // you can add any code you want within this function scope
 
-  function substitution(input, alphabet, encode = true) {
-    // your solution code here
-  }
+    function substitution(input, alphabet, encode = true) {
+        if (alphabet === undefined) return false;
+        if (alphabet.length != 26) return false;
+        for (let char of alphabet) {
+            if (alphabet.indexOf(char) != alphabet.lastIndexOf(char))
+                return false;
+        }
+        let normalAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        let encodedMessage = "";
+        input = input.toLowerCase();
 
-  return {
-    substitution,
-  };
+        for (let char of input) {
+            if (char === " ") {
+                encodedMessage += " ";
+                continue;
+            }
+            encode
+                ? (encodedMessage += alphabet[normalAlphabet.indexOf(char)])
+                : (encodedMessage += normalAlphabet[alphabet.indexOf(char)]);
+        }
+        return encodedMessage;
+    }
+
+    return {
+        substitution,
+    };
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
--- Project_Decoder_Ring_1/test/substitution.test.js
+++ Project_Decoder_Ring_1/test/substitution.test.js
@@ -1,1 +1,27 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const substitutionModule = require("../src/substitution");
+
+describe("", () => {
+    it("should return false if the alphabet parameter is missing", () => {
+        const encryptedMessage = substitutionModule.substitution("thinkful");
+        const expected = false;
+        expect(encryptedMessage).to.equal(expected);
+    }),
+        it("should return false if the alphabet parameter is not 26 characters long", () => {
+            const encryptedMessage = substitutionModule.substitution(
+                "thinkful",
+                "short"
+            );
+            const expected = false;
+            expect(encryptedMessage).to.equal(expected);
+        }),
+        it("should return false if the characters in alphabet parameter are not unique", () => {
+            const encryptedMessage = substitutionModule.substitution(
+                "thinkful",
+                "abcabcabcabcabcabcabcabcyz"
+            );
+            const expected = false;
+            expect(encryptedMessage).to.equal(expected);
+        });
+});
