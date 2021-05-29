df1675df83629ce5fe58f473abc92aa3d7ada907
Yevgen Sydoruk <yevgen.sydoruk@gmail.com>
2021-05-29 08:55:47 -0500

    polybius: Test cases done

--- Project_Decoder_Ring_1/test/polybius.test.js
+++ Project_Decoder_Ring_1/test/polybius.test.js
@@ -1,1 +1,16 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const polybiusModule = require("../src/polybius");
+
+describe("polybius", () => {
+    it("should return false if the number of characters in the string excluding spaces is not even", () => {
+        const encryptedMessage = polybiusModule.polybius("11222", false);
+        const expected = false;
+        expect(encryptedMessage).to.equal(expected);
+    }),
+        it("should return false if the output is not a string", () => {
+            const encryptedMessage = polybiusModule.polybius("112233");
+            const expected = false;
+            expect(encryptedMessage).to.be.a("string");
+        });
+});
